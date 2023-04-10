cc248518bccf912463cbff8132467a0b57cf3f13
Blake Lamb <blake.lamb31@gmail.com>
2023-04-09 21:56:27 -0600

    Changed to use the observables instead of behaviorsubject

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
 import {TodoApiService} from './todo-api/todo-api.service'
-import {BehaviorSubject} from 'rxjs';
+import {BehaviorSubject, Observable} from 'rxjs';
 
 
 @Component({
@@ -13,26 +13,19 @@ export class AppComponent {
   singleItem: string = ''
 
 
-  displayList$: BehaviorSubject<any> = new BehaviorSubject<any>([])
+  displayList$: Observable<any> = this._todos.getTodos()
 
 
   constructor(private _todos: TodoApiService) { }
 
-  ngOnInit() {
-    this._todos.getTodos().pipe(
-    ).subscribe(list => this.displayList$.next(list))
-  }
+  ngOnInit() { }
 
   addItem(todo: string): void {
-    this._todos.createTodo(todo).subscribe((s) => {
-      this.displayList$.next(s)
-    })
+    this.displayList$ = this._todos.createTodo(todo)
   }
 
   markComplete(id: string): void {
-    this._todos.markComplete(id).subscribe((s) => {
-      this.displayList$.next(s)
-    })
+    this.displayList$ = this._todos.markComplete(id)
   }
 
 }
